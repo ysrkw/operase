39a6baf8b8872cb5776081e499c6068abdfb2ab2
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-08-17 14:32:57 +0000

    feat: ユーザーリポジトリデータベースサービスを追加し、ユーザーとパスワードの保存機能を実装

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,15 @@
 import { drizzle } from 'drizzle-orm/libsql'
 import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const database = drizzle('file:dist/local.db')
+export const database = drizzle(':memory:')
 
 export const users = sqliteTable('users', {
-  id: text('id').primaryKey(),
-})
-
-export const userEmails = sqliteTable('user_emails', {
   email: text('email').notNull().unique(),
-  userId: text('user_id').notNull().references(() => users.id),
+  id: text('id').notNull().primaryKey(),
 })
 
 export const userPasswords = sqliteTable('user_passwords', {
+  createdAt: text('created_at').notNull().default('now'),
   hash: text('hash').notNull(),
-  type: text('type').notNull().default('argon2'),
   userId: text('user_id').notNull().references(() => users.id),
 })

e6fbb268d138449bcfb60798b05d7c6839063b65
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-08-17 14:39:15 +0000

    feat: Effect.promiseをEffect.tryPromiseに変更し、トランザクションのエラーハンドリングを改善

--- src/modules/invite/services/user-repository-database.ts
+++ src/modules/invite/services/user-repository-database.ts
@@ -4,12 +4,11 @@ import { database, userPasswords, users } from '../../../database'
 import { UserRepositoryTag } from './user-repository'
 
 export const UserRepositoryDatabase = UserRepositoryTag.of({
-  save: user => Effect.promise(() => database.transaction(async (t) => {
+  save: user => Effect.tryPromise(() => database.transaction(async (t) => {
     await t.insert(users).values({
       email: user.email,
       id: user.id,
     })
-
     await t.insert(userPasswords).values({
       hash: user.hashedPassword,
       userId: user.id,

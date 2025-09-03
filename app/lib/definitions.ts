ea111978e09b6e738a602e2feeba6ecf51736432
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-09-03 17:51:09 +0000

    feat: Id、ConfirmPassword、HashedPasswordの型定義を追加

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod'
 
+export const Id = z.ulid().brand<'Id'>()
+export type Id = z.infer<typeof Id>
+
 export const Name = z.string().min(1).max(255).brand<'Name'>()
 export type Name = z.infer<typeof Name>
 
@@ -9,5 +12,8 @@ export type Email = z.infer<typeof Email>
 export const Password = z.string().min(8).max(64).brand<'Password'>()
 export type Password = z.infer<typeof Password>
 
-export const ConfirmPassword = Password.clone().brand<'ConfirmPassword'>()
+export const ConfirmPassword = z.string().min(8).max(64).brand<'ConfirmPassword'>()
 export type ConfirmPassword = z.infer<typeof ConfirmPassword>
+
+export const HashedPassword = z.string().nonempty().brand<'HashedPassword'>()
+export type HashedPassword = z.infer<typeof HashedPassword>

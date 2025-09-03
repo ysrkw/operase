0a1e3b3a8509dd3d12e1f00a581884893ded5f01
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-09-03 07:23:24 +0000

    feat: zodのブランドを追加して型の明確性を向上

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
-export const Name = z.string().min(1).max(255)
+export const Name = z.string().min(1).max(255).brand<'Name'>()
 export type Name = z.infer<typeof Name>
 
-export const Email = z.email()
+export const Email = z.email().brand<'Email'>()
 export type Email = z.infer<typeof Email>
 
-export const Password = z.string().min(8).max(64)
+export const Password = z.string().min(8).max(64).brand<'Password'>()
 export type Password = z.infer<typeof Password>
 
-export const ConfirmPassword = Password.clone()
+export const ConfirmPassword = Password.clone().brand<'ConfirmPassword'>()
 export type ConfirmPassword = z.infer<typeof ConfirmPassword>

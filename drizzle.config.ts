52f9a73276c9af52d4edf8e7d82347feb8bc0c3f
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-09-09 03:59:38 +0000

    feat: drizzle.config.tsの設定を更新し、schema.tsにユーザー名とセッションテーブルを追加

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'postgresql',
+  driver: 'pglite',
+  out: './lib/database/migrations',
   schema: './lib/database/schema.ts',
 })
--- lib/database/schema.ts
+++ lib/database/schema.ts
@@ -3,10 +3,17 @@ import { pgTable, varchar } from 'drizzle-orm/pg-core'
 export const users = pgTable('users', {
   email: varchar().notNull().unique(),
   id: varchar().notNull().primaryKey(),
+  name: varchar().notNull(),
 })
 
 export const userPasswords = pgTable('user_passwords', {
   createdAt: varchar().notNull().default('now'),
   hash: varchar().notNull().primaryKey(),
+  userId: varchar().notNull().references(() => users.id),
+})
+
+export const userSessions = pgTable('user_sessions', {
+  createdAt: varchar().notNull().default('now'),
+  id: varchar().notNull().primaryKey(),
   userId: varchar().notNull().references(() => users.id),
 })

4fe033058d9c7a2915bf1f1fb0a55027ae1df1c0
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-09-09 05:24:43 +0000

    feat: グループテーブルを追加し、メンバーシップとセッションのスキーマを更新

--- lib/database/schema.ts
+++ lib/database/schema.ts
@@ -1,18 +1,28 @@
 import { pgTable, varchar } from 'drizzle-orm/pg-core'
 
+export const group = pgTable('group', {
+  id: varchar().notNull().primaryKey(),
+  name: varchar().notNull(),
+})
+
 export const users = pgTable('users', {
   email: varchar().notNull().unique(),
   id: varchar().notNull().primaryKey(),
   name: varchar().notNull(),
 })
 
-export const userPasswords = pgTable('user_passwords', {
+export const memberships = pgTable('memberships', {
+  groupId: varchar().notNull().references(() => group.id),
+  userId: varchar().notNull().references(() => users.id),
+})
+
+export const passwords = pgTable('passwords', {
   createdAt: varchar().notNull().default('now'),
   hash: varchar().notNull().primaryKey(),
   userId: varchar().notNull().references(() => users.id),
 })
 
-export const userSessions = pgTable('user_sessions', {
+export const sessions = pgTable('sessions', {
   createdAt: varchar().notNull().default('now'),
   id: varchar().notNull().primaryKey(),
   userId: varchar().notNull().references(() => users.id),

fdea23f38a7bff7043f0779e856d90940432b08b
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-09-27 16:18:57 +0000

    refactor: 不要なサービスおよびモデルファイルを削除し、コードベースをクリーンアップ

--- src/apps/invite/drizzle-service.ts
+++
@@ -1,8 +0,0 @@
-import { PgliteDatabase } from 'drizzle-orm/pglite'
-import { Context, Effect } from 'effect'
-
-export interface DrizzleService {
-  getDatabase: () => Effect.Effect<PgliteDatabase, Error>
-}
-
-export const DrizzleService = Context.GenericTag<DrizzleService>('DrizzleService')

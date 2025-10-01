8cc2fd11e51438dd5235004eaa0c8f01eeac57e2
ysrkw <15919660+ysrkw@users.noreply.github.com>
2025-10-01 13:41:58 +0000

    refactor: SignInFormの構造を改善し、fieldsetを追加して可読性を向上

--- app/sign-in/components/sign-in-form.tsx
+++ app/sign-in/components/sign-in-form.tsx
@@ -10,28 +10,33 @@ export function SignInForm() {
   const passwordId = useId()
 
   return (
-    <form action={signIn} className="flex flex-col gap-4">
-      <div className="flex flex-col gap-1">
-        <label htmlFor={emailId}>メールアドレス</label>
-        <input className="rounded border px-4 py-2" id={emailId} name="email" placeholder="john@example.com" required type="email" />
-      </div>
-      <div className="flex flex-col gap-1">
-        <label htmlFor={passwordId}>パスワード</label>
-        <input className="rounded border px-4 py-2" id={passwordId} name="password" required type="password" />
-        <div className="text-right">
-          <Link className="text-sm underline" href="/forgot-password">
-            パスワードをお忘れですか？
+    <form action={signIn}>
+      <fieldset className="flex flex-col gap-4">
+        <legend className="text-center text-xl">
+          ログイン
+        </legend>
+        <div className="flex flex-col gap-1">
+          <label htmlFor={emailId}>メールアドレス</label>
+          <input className="rounded border px-4 py-2" id={emailId} name="email" placeholder="john@example.com" required type="email" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor={passwordId}>パスワード</label>
+          <input className="rounded border px-4 py-2" id={passwordId} name="password" required type="password" />
+          <div className="text-right">
+            <Link className="text-sm underline" href="/forgot-password">
+              パスワードをお忘れですか？
+            </Link>
+          </div>
+        </div>
+        <button className="cursor-pointer rounded-full border px-4 py-2 text-center" type="submit">
+          ログインに進む
+        </button>
+        <div className="text-center">
+          <Link className="text-sm underline" href="/sign-up">
+            アカウントをお持ちでない場合はこちら
           </Link>
         </div>
-      </div>
-      <button className="cursor-pointer rounded-full border px-4 py-2 text-center" type="submit">
-        ログイン
-      </button>
-      <div className="text-center">
-        <Link className="text-sm underline" href="/sign-up">
-          アカウントをお持ちでない場合はこちら
-        </Link>
-      </div>
+      </fieldset>
     </form>
   )
 }
